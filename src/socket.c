3bdddf98e9362c2781566b46ae6c7eb733df6770
Martin Pasture <33912552+MartiMcFlyP@users.noreply.github.com>
2019-10-22 09:45:33 +0200

    Add files via upload

--- src/socket.c
+++ src/socket.c
@@ -15,7 +15,7 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-const char * real_address(const char *address, struct sockaddr_in6 *rval) {
+const char * decode_address(const char *address, struct sockaddr_in6 *rval) {
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET6; //Adresse IPV6
@@ -23,9 +23,9 @@ const char * real_address(const char *address, struct sockaddr_in6 *rval) {
     hints.ai_protocol = IPPROTO_UDP; //Proctocole UDP
     hints.ai_socktype = SOCK_DGRAM; //UDP utilise DGRAM
     struct addrinfo *res;
-    int s = getaddrinfo(address, NULL, &hints, & res); //transforme l'adresse reçue en adresse IPV6
+    int s = getaddrinfo(address, NULL, &hints, & res); //transforme l'adresse reçue en adresse IPV6/DATAGRAM
     if(s != 0){
-        return gai_strerror(s);
+        return gai_strerror(s);//renvoie l'erreur sous forme compréhensible
     }
     rval = (struct sockaddr_in6*)(res->ai_addr);
     freeaddrinfo(res);
@@ -33,29 +33,30 @@ const char * real_address(const char *address, struct sockaddr_in6 *rval) {
 }
 
 int create_socket(struct sockaddr_in6 *source_addr, int src_port, struct sockaddr_in6 *dest_addr,int dst_port){
+
     int sockfd;
-    int s;
     sockfd = socket(AF_INET6,SOCK_DGRAM,IPPROTO_UDP);
-    if (sockfd == -1){
-        perror("socket failed");
+    if (sockfd < 0){
+        perror("socket creation failed");
         return -1;
     }
+
     if(source_addr != NULL && src_port > 0){
         source_addr->sin6_port = htons(src_port);
-        s = bind(sockfd,(struct sockaddr *)source_addr, sizeof(struct sockaddr_in6));
-        if (s != 0){
+        if (bind(sockfd,(struct sockaddr *)source_addr, sizeof(struct sockaddr_in6)) == -1){
             perror("bind failed");
             return -1;
         }
     }
+
     if (dest_addr != NULL && dst_port >0){
         dest_addr->sin6_port = htons(dst_port);
-        s = connect(sockfd,(struct sockaddr *)dest_addr, sizeof(struct sockaddr_in6));
-        if (s != 0){
+        if (connect(sockfd,(struct sockaddr *)dest_addr, sizeof(struct sockaddr_in6)) == -1){
             perror("connection failed");
             return -1;
         }
     }
+
     return sockfd;
 }
 
@@ -65,15 +66,13 @@ int wait_for_client(int sfd){
     socklen_t len = sizeof(struct sockaddr_in6);
     memset(&caller_addr,0,len);
     char buf[1024];
-    int nread;
-    nread = recvfrom(sfd, buf, sizeof(buf), 0, (struct sockaddr *)&caller_addr, &len); //vérifier le bon fonctionnement
-    if (nread == -1){
+    if (recvfrom(sfd, buf, sizeof(buf), 0, (struct sockaddr *)&caller_addr, &len) == -1){//vérifier le bon fonctionnement
         perror("error while receiving");
         return -1;
     }
-    int s = connect(sfd, (struct sockaddr *)&caller_addr, len);
-    if(s == -1){
+    if(connect(sfd, (struct sockaddr *)&caller_addr, len) == -1){
         perror("error while connecting");
+        return -1;
     }
-    return s;
+    return 0;
 }
