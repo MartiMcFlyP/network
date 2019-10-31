8519b0c0ec6e6bb45921017fb40b1ea6b663df36
tbaldwinUCL <48825885+tbaldwinUCL@users.noreply.github.com>
2019-10-31 13:24:40 +0100

    Update receive.c

--- src/receive.c
+++ src/receive.c
@@ -1,6 +1,5 @@
 #include "socket.h"
 #include "packet_implement.h"
-#include "receive.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -9,177 +8,37 @@
 #include <sys/time.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <netinet/in.h> 
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h> /* * sockaddr_in6 */
 #include <netdb.h>
 #include <math.h>
 #include <unistd.h>
 #include <errno.h>
 
 
-/* !!! Attention, ce code est grandement inspiré d'un code trouvé sur internet !!! */
+
+
+
+
+
+
+/*A FAIRE ! 
+selective repeat
+
+calcul CRC
+commentaires
+*/
+
+
+
+
+
+
 
 char *buffer_payload[MAX_PAYLOAD_SIZE]; //Permet de stocker les payload recu
 int buffer_len[MAX_WINDOW_SIZE]; //Permet de stocker la taille des payload recu
-
-void receive_data(char* hostname, int port, int N){
-
-    struct sockaddr_in6 addr;
-    memset(&addr, 0, sizeof(addr));
-    //Récupération  l'address en IPV6
-    const char* ret_real = decode_address(hostname, &addr);
-
-    if (ret_real != NULL){
-        fprintf(stderr, "Address '%s' is not recognized.", hostname);
-        return;
-    }
-
-    //Création du socket en fournissant l'adresse et le port du receiver fournit par l'utilisateur
-    int socket_fd = create_socket(&addr, port, NULL, 0);
-
-    if(socket_fd == -1){
-        return;
-    }
-
-    //On attend une connection
-    int wait = wait_for_client(socket_fd);
-
-    if(wait == -1){
-        return;
-    }
-
-    //Permet de lire les packet recu
-    pkt_t* pkt_rcv;
-    //Permet d'envoyer des ACK/NACK
-    pkt_t* pkt_ack;
-
-
-    struct timeval tv;
-    tv.tv_sec = 5;
-    tv.tv_usec = 0;
-
-    int window = 3;
-    int index = 0; //Utilisé pour gerer les indice du buffer
-    int seq_exp = 0; //Numéro de segment attendu
-
-    memset(buffer_len,-1,MAX_WINDOW_SIZE);
-    char packet_encoded[528];
-
-
-    pkt_rcv = pkt_new();
-    if(pkt_rcv == NULL){
-        fprintf(stderr, "An occur failed while creating a data packet.");
-        pkt_del(pkt_rcv);
-        return;
-    }
-
-    pkt_ack = pkt_new();
-    if(pkt_ack == NULL){
-        fprintf(stderr, "An occur failed while creating a data packet.");
-        pkt_del(pkt_ack);
-        pkt_del(pkt_rcv);
-        return;
-    }
-
-
-    fd_set readset;
-
-    FD_ZERO(&readset);
-    FD_SET(socket_fd, &readset);
-    int result = select(socket_fd + N-1, &readset, NULL, NULL, &tv);
-
-    while (result == -1 && errno == EINTR){
-
-        if (result > 0) {
-            if (FD_ISSET(socket_fd, &readset)) {
-                /* The socket_fd has data available to be read */
-                result = read(socket_fd, packet_encoded, 528);
-
-                if (result == 0) {
-                    /* This means the other side closed the socket */
-                    close(socket_fd);
-                }
-
-                else if(result < 0){
-                    pkt_del(pkt_rcv);
-                    pkt_del(pkt_ack);
-                }
-
-                else if(result >0);
-                //Décodage du packet(on a besoin que de packet contenant de la data)
-                if(pkt_decode((const char*)packet_encoded,(int)result,pkt_rcv) == PKT_OK && pkt_get_type(pkt_rcv) == PTYPE_DATA){
-                    int seq_rcv = pkt_get_seqnum(pkt_rcv);
-
-                    //Si tr == 1 => on envoie un NACK
-                    if(pkt_get_tr(pkt_rcv) == 1){
-                        if(send_ack(pkt_ack,seq_rcv,socket_fd, PTYPE_NACK, pkt_get_timestamp(pkt_rcv),window) < 0){
-                            fprintf(stderr,"Error sending nack");
-                        }
-
-                    }
-
-                    else{
-                        //Si on recooit un pack de taille 0 -> Indique la fin du transfert
-                        int leng = pkt_get_length(pkt_rcv);
-
-                        if(leng == 0)
-                        {
-                            printf("[[[ EOF RECEIVED ]]]\n");
-                        }
-
-                        else
-                        {
-
-                            //Ajout du packet recu dans un buffer (pour gerer les cas ou on a recu
-                            //un packet avec un numéro de segment supérieur au numéro de segment attendu
-                            add_buffer(index, seq_rcv, seq_exp, pkt_rcv, window);
-                            //Ecriture de buffer et on le vide si le packet avec le bon numéro de segment attendu
-                            //a bien été reçu
-                            while (buffer_len[index] != -1){
-                                //Ecriture le pyaload des packets dans le fichier
-                                if(write(&readset,buffer_payload[index],buffer_len[index]) < 0)
-                                {
-                                    fprintf(stderr,"ERROR WRITING PACKET");
-                                }
-                                //Réinitialisation du payload écrit
-                                buffer_payload[index] = (char *)NULL;
-                                buffer_len[index] = -1;
-                                //On passe à l'index suivant (l'index ne peut jamais dépasser la window size).
-                                index = (index+1)%window;
-                                //Le numéro de séquence attendu est incrémenté
-                                seq_exp = (seq_exp+1)%256;
-                            }
-                            if(send_ack(pkt_ack,seq_exp-1,socket_fd, PTYPE_ACK, pkt_get_timestamp(pkt_rcv), window) < 0)
-                            {
-                                fprintf(stderr,"Error sending ack");
-                            }
-
-                        }
-
-                    }
-                }
-                else
-                {
-                    printf("NACKNACK\n");
-                }
-            }
-        }
-        else if (result < 0) {
-            /* An error ocurred, just print it to stdout */
-            printf("Error on select(): %s\"", strerror(errno));
-        }
-
-    }
-
-
-    close(socket_fd);
-    close(&readset);
-    pkt_del(pkt_ack);
-    pkt_del(pkt_rcv);
-}
-
 
 
 void add_buffer(int index, int seq_rcv, int seq_exp, pkt_t * pkt_rcv, int window){
@@ -202,6 +61,8 @@ void add_buffer(int index, int seq_rcv, int seq_exp, pkt_t * pkt_rcv, int window
         buffer_payload[real_ind] = (char *)pkt_get_payload(pkt_rcv);
     }
 }
+
+
 
 
 int send_ack(pkt_t *pkt_ack, int seqnum, int socket_fd, int ack, uint32_t time_data, int window){
@@ -243,18 +104,18 @@ int send_ack(pkt_t *pkt_ack, int seqnum, int socket_fd, int ack, uint32_t time_d
         return -1;
     }
 
-    char buf[20];
+    char buff[20];
     size_t buf_len = 20;
 
     //Encodage du ACK/NACK et remplissage de la variable buf
-    return_status = pkt_encode_ack(pkt_ack, buf, &buf_len);
+    return_status = pkt_encode_ack(pkt_ack, buff, &buf_len);
     if(return_status != PKT_OK){
         perror("Encodage de l'acknowledge");
         return -1;
     }
 
     //On envoie le ack/nack encodé
-    if(write(socket_fd, buf, buf_len) < 0)
+    if(write(socket_fd, buff, buf_len) < 0)
     {
         perror("Encodage write ack");
         return -1;
@@ -262,3 +123,216 @@ int send_ack(pkt_t *pkt_ack, int seqnum, int socket_fd, int ack, uint32_t time_d
 
     return 0;
 }
+
+
+
+
+
+void receiver(char* hostname, int port, int N, char* format){
+
+    struct sockaddr_in6 addr;
+    memset(&addr, 0, sizeof(addr));
+    //Récupération  l'address en IPV6
+    const char* ret_real = decode_address(hostname, &addr);
+
+    if (ret_real != NULL){
+        fprintf(stderr, "Address '%s' is not recognized.", hostname);
+        return;
+    }
+
+    //Création du socket serveur
+    int socket_fd = create_socket(&addr, port, NULL, 0);
+    if(socket_fd == -1){
+        return;
+    }
+
+
+    //Permet de lire les packet recu
+    pkt_t* pkt_rcv;
+    //Permet d'envoyer des ACK/NACK
+    pkt_t* pkt_ack;
+
+
+    int window = 10;
+    int index = 0; //Utilisé pour gerer les indice du buffer
+    int seq_attendue = 0; //Numéro de segment attendu
+
+    memset(buffer_len,-1,MAX_WINDOW_SIZE);
+    char packet_encoded[528];
+
+
+    pkt_rcv = pkt_new();
+    if(pkt_rcv == NULL){
+        fprintf(stderr, "Erreur creation packet");
+        pkt_del(pkt_rcv);
+        return;
+    }
+
+    pkt_ack = pkt_new();
+    if(pkt_ack == NULL){
+        fprintf(stderr, "Erreur creation packet");
+        pkt_del(pkt_ack);
+        pkt_del(pkt_rcv);
+        return;
+    }
+
+    //ouverture du fichier de sortie     
+    char file[sizeof(format)];
+    sprintf(file,format,0); //sends formatted output to a string pointed to, by str
+    int fd = open((const char *)file, O_WRONLY | O_CREAT | O_TRUNC , S_IRWXU);
+    if(fd == -1){
+        fprintf(stderr, "An occur failed while opening fd.");
+        return;
+    }
+
+    fd_set readset;     //liste de fd
+    
+
+    int listen_socket = listen(socket_fd,N);
+    int activity;
+    int sender_socket [N];
+    struct sockaddr_in client_address;
+
+    while (1) {
+
+
+        FD_ZERO(&readset);  //on le met à 0 
+        FD_SET(socket_fd, &readset);    //on ajoute le socket serveur
+
+        //wait for an activity on one of the sockets , timeout is NULL , so wait indefinitely
+        activity = select(socket_fd + N-1 , &readset , NULL , NULL , NULL);
+   
+        if ((activity == -1) && (errno == EINTR)) 
+        {
+            continue;
+        }
+        
+        if (activity == -1) {
+            perror("probleme select()");
+            exit(EXIT_FAILURE);
+        }
+
+        //If something happened on the  socket , then its an incoming connection
+        if (FD_ISSET(socket_fd, &readset)) {
+            int new_socket = accept(socket_fd, (struct sockaddr *) &client_address, sizeof(client_address));
+            sender_socket[i] == new_socket;
+            FD_SET(new_socket, &readset);
+        }
+
+        for( int i = 0; i < N; i++){
+            if(FD_ISSET(sender_socket[i]), &readset){
+            
+                int reader = recv(sender_socket[i], packet_encoded, 528, 0);
+                if (reader == -1) {
+                    close(sender_socket[i]);
+                }
+
+
+                else if(reader >0){
+                    //Décodage du packet(on a besoin que de packet contenant de la data)
+                    if(pkt_decode((const char*)packet_encoded,(int)reader,pkt_rcv) == PKT_OK && pkt_get_type(pkt_rcv) == PTYPE_DATA){
+                        int seq_rcv = pkt_get_seqnum(pkt_rcv);
+
+                        if(pkt_get_tr(pkt_rcv) == 1){ //paquet tronqué donc envoi de NACK
+                            if(send_ack(pkt_ack,seq_rcv,socket_fd, PTYPE_NACK, pkt_get_timestamp(pkt_rcv),window) < 0){
+                                fprintf(stderr,"Error sending nack");
+                            }
+
+                        }
+
+                        else{
+                
+                            int leng = pkt_get_length(pkt_rcv);
+
+                            if(leng == 0)
+                            {
+                                printf("Fermeture du socket [%d]\n", i);
+                                if(close(sender_socket[i]) == -1){
+                                    fprintf(stderr,"Erreur fermeture socket");
+                                }
+                            }
+
+                            else
+                            {
+                                add_buffer(index, seq_rcv, seq_attendue, pkt_rcv, window);
+                                
+                                while (buffer_len[index] != -1){
+                                    if(write(fd,buffer_payload[index],buffer_len[index]) < 0)
+                                    {
+                                        fprintf(stderr,"erreur écriture");
+                                    }
+                                    buffer_payload[index] = (char *)NULL;
+                                    buffer_len[index] = -1;
+                                    //On passe à l'index suivant (l'index ne peut jamais dépasser la window size).
+                                    index = (index+1)%window;
+                                    //Le numéro de séquence attendu est incrémenté
+                                    seq_attendue = (seq_attendue+1)%256;
+                                }
+                                if(send_ack(pkt_ack,(seq_attendue)-1,socket_fd, PTYPE_ACK, pkt_get_timestamp(pkt_rcv), window) < 0)
+                                {
+                                    fprintf(stderr,"Error sending ack\n");
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+                else
+                {
+                    if(close(sender_socket[i]) == -1){
+                        fprintf(stderr,"Erreur fermeture socket");
+                    }
+                }
+            }
+        }
+    }
+
+
+
+   
+}
+
+
+int main(int argc, char **argv) {
+    int opt;
+    int port;
+    int N = 1;
+    char *format = NULL;
+    char *hostname;
+    while ((opt = getopt(argc, argv, "o:m:")) != -1) {
+        switch (opt) {
+            case 'o':
+                format = optarg;
+                break;
+            case 'm':
+                N = atoi(optarg);
+                break;
+            case '?':
+                if(optopt == 'c') {
+                    fprintf(stderr, "Option -%c doit prendre un argument.\n", optopt);
+                }
+                else{
+                    fprintf(stderr, "caractère d'option inconnue '\\x%x'.\n",optopt);
+                }
+            default: /* */
+                abort();
+        }
+    }
+    if (optind+2 != argc) {
+        fprintf(stderr, "Nombre d'arguments incorrect\n");
+        exit(EXIT_FAILURE);
+    }
+    hostname = argv[optind];
+    port = atoi(argv[optind+1]);
+    receiver(hostname, port, N, format);
+    exit(EXIT_SUCCESS);
+}
+
+
+
+
+
+
+
+
