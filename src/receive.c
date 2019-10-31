1fa6333782d820afbc4de1b77c03288f25172582
Martin Pasture <33912552+MartiMcFlyP@users.noreply.github.com>
2019-10-31 16:04:38 +0100

    Update receive.c

--- src/receive.c
+++ src/receive.c
@@ -1,6 +1,4 @@
-
-  
-  #include "socket.h"
+#include "socket.h"
 #include "packet_implement.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -18,89 +16,65 @@
 #include <math.h>
 #include <unistd.h>
 #include <errno.h>
-
-
-
-
-
-
-
-
-/*A FAIRE ! 
-selective repeat
-acq seq
-calcul CRC
-commentaires
-*/
-
-
-
+#include <sys/select.h>
 
 int send_ack(pkt_t *pkt_ack, int seqnum, int socket_fd, int ack, uint32_t time_data, int window){
 
     pkt_status_code return_status;
-    //Etablissement des valeurs du ack
-    return_status = pkt_set_seqnum(pkt_ack, seqnum+1);
-
-    if(return_status != PKT_OK){    //problème au set
+    return_status = pkt_set_seqnum(pkt_ack, seqnum+1);                                  //construction des valeurs du ack
+    if(return_status != PKT_OK){
         perror("Creation de l'acknowledge : ");
         return -1;
     }
-
-    // type à PTYPE_ACK ou PTYPE_NACK
-    if(ack == PTYPE_ACK){
+    if(ack == PTYPE_ACK){                                                               //type ACK
         return_status = pkt_set_type(pkt_ack, PTYPE_ACK);
     }
-
     else if(ack == PTYPE_NACK)
-        return_status = pkt_set_type(pkt_ack, PTYPE_NACK);
-
-    if(return_status != PKT_OK){        //problème au set
+        return_status = pkt_set_type(pkt_ack, PTYPE_NACK);                              //type NACK
+    if(return_status != PKT_OK){
         perror("Creation de l'acknowledge : ");
         return -1;
     }
     return_status = pkt_set_timestamp(pkt_ack, time_data);
-
-
+    if(return_status != PKT_OK){
+        perror("Creation de l'acknowledge : ");
+        return -1;
+    }
     return_status = pkt_set_window(pkt_ack, window);
     if(return_status != PKT_OK){
         perror("Creation de l'acknowledge : ");
         return -1;
     }
-
-    //Les ack/nack n'ont pas de payload
     return_status = pkt_set_payload(pkt_ack, NULL, 0);
     if(return_status != PKT_OK){
         perror("Creation de l'acknowledge : ");
         return -1;
     }
-
     char buff[20];
     size_t buf_len = 20;
-
-    //Encodage du ACK/NACK et remplissage de la variable buf
     return_status = pkt_encode_ack(pkt_ack, buff, &buf_len);
     if(return_status != PKT_OK){
         perror("Encodage de l'acknowledge");
         return -1;
     }
-
-    //On envoie le ack/nack encodé
     if(write(socket_fd, buff, buf_len) < 0)
     {
         perror("Encodage write ack");
         return -1;
     }
-
     return 0;
 }
 
 
 void receiver(char* hostname, int port, int N, char* format){
 
+    char ***buffer_payload[N][MAX_PAYLOAD_SIZE];                                    //Permet de stocker les payload recu
+    int buffer_len[N][MAX_WINDOW_SIZE];                                             //Permet de stocker la taille des payload recu
+    int buffer_seq[N][MAX_WINDOW_SIZE];                                             // Permet de stocker les num de seq attendues
+
     struct sockaddr_in6 addr;
     memset(&addr, 0, sizeof(addr));
-    //Récupération  l'address en IPV6
+                                                                                    //Récupération  l'address en IPV6
     const char* ret_real = decode_address(hostname, &addr);
 
     if (ret_real != NULL){
@@ -108,22 +82,15 @@ void receiver(char* hostname, int port, int N, char* format){
         return;
     }
 
-    //Création du socket serveur
+                                                                                    //Création du socket serveur
     int socket_fd = create_socket(&addr, port, NULL, 0);
     if(socket_fd == -1){
         return;
     }
-
-
-    //Permet de lire les packet recu
-    pkt_t* pkt_rcv;
-    //Permet d'envoyer des ACK/NACK
+    pkt_t* pkt_rcv;                                                                 //Creation des paquets recu et envoye
     pkt_t* pkt_ack;
-
     memset(buffer_len,-1,MAX_WINDOW_SIZE);
     char packet_encoded[528];
-
-
     pkt_rcv = pkt_new();
     if(pkt_rcv == NULL){
         fprintf(stderr, "Erreur creation packet");
@@ -138,36 +105,36 @@ void receiver(char* hostname, int port, int N, char* format){
         pkt_del(pkt_rcv);
         return;
     }
-
-    //ouverture du fichier de sortie     
+                                                                                    //ouverture du fichier de sortie
     char file[sizeof(format)];
-    sprintf(file,format,0); //sends formatted output to a string pointed to, by str
+    sprintf(file,format,0);                                                         //envois une reponse formate
     int fd = open((const char *)file, O_WRONLY | O_CREAT | O_TRUNC , S_IRWXU);
     if(fd == -1){
         fprintf(stderr, "An occur failed while opening fd.");
         return;
     }
 
-    fd_set readset;     //liste de fd
+    fd_set readset;                                                                 //liste de fd
     
 
     int window = 10;
-    int index = 0; //Utilisé pour gerer les indice du buffer
     int listen_socket = listen(socket_fd,N);
+    if(listen_socket == -1 ){
+        fprintf(stderr, "Fail");
+    }
     int activity;
-    int sender_socket [N];
+    int sender_socket[N];
     struct sockaddr_in client_address;
+    int adress_len = sizeof(client_address);
     int seq_attendue[N]; //seq attendue
 
-    char **buffer_payload[N][MAX_PAYLOAD_SIZE]; //Permet de stocker les payload recu
-    int *buffer_len[N][MAX_WINDOW_SIZE]; //Permet de stocker la taille des payload recu
-    int *buffer_seq[N][MAX_WINDOW_SIZE]; // Permet de stocker les num de seq attendues
+
 
     while (1) {
 
 
-        FD_ZERO(&readset);  //on le met à 0 
-        FD_SET(socket_fd, &readset);    //on ajoute le socket serveur
+        FD_ZERO(&readset);                                                          //on le met à 0
+        FD_SET(socket_fd, &readset);                                                //on ajoute le socket serveur
 
         //wait for an activity on one of the sockets , timeout is NULL , so wait indefinitely
         activity = select(socket_fd + N-1 , &readset , NULL , NULL , NULL);
@@ -184,13 +151,18 @@ void receiver(char* hostname, int port, int N, char* format){
 
         //If something happened on the  socket , then its an incoming connection
         if (FD_ISSET(socket_fd, &readset)) {
-            int new_socket = accept(socket_fd, (struct sockaddr *) &client_address, sizeof(client_address));
-            sender_socket[i] == new_socket;
+            int new_socket = accept(socket_fd, (struct sockaddr *) &client_address, &adress_len);
+            for (int i = 0; i < N; ++i) {
+                if(sender_socket[i]==0){
+                    sender_socket[i] == new_socket;
+                    break;
+                }
+            }
             FD_SET(new_socket, &readset);
         }
 
         for( int i = 0; i < N; i++){
-            if(FD_ISSET(sender_socket[i]), &readset){
+            if(FD_ISSET(sender_socket[i], &readset)){
             
                 int reader = recv(sender_socket[i], packet_encoded, 528, 0);
                 if (reader == -1) {
@@ -268,40 +240,44 @@ void receiver(char* hostname, int port, int N, char* format){
 }
 
 
-int main(int argc, char **argv) {
-    int opt;
-    int port;
-    int N = 1;
+int main(int argc, char **argv) {                                                                   //Commence le programme
+    int opt;                                                                                        //option courante
+    int port;                                                                                       //Port de connexion
+int N = 1;                                                                                          //Nombre de connexions simultanee maximale
     char *format = NULL;
     char *hostname;
-    while ((opt = getopt(argc, argv, "o:m:")) != -1) {
+    while ((opt = getopt(argc, argv, "o:m:")) != -1) {                                              //On parcours les options
         switch (opt) {
             case 'o':
-                format = optarg;
+                format = optarg;                                                                    //le format est récupéré ici
                 break;
             case 'm':
-                N = atoi(optarg);
+                N = atoi(optarg);                                                                   //le nombre de connections est récupéré ici
                 break;
             case '?':
                 if(optopt == 'c') {
-                    fprintf(stderr, "Option -%c doit prendre un argument.\n", optopt);
+                    fprintf(stderr, "Option -%c doit prendre un argument.\n", optopt);              //l'option n'est pas pourvue d'un argument
                 }
                 else{
-                    fprintf(stderr, "caractère d'option inconnue '\\x%x'.\n",optopt);
+                    fprintf(stderr, "caractère d'option inconnue '\\x%x'.\n",optopt);               //l'option n'est pas reconnue
                 }
             default: /* */
                 abort();
         }
     }
-    if (optind+2 != argc) {
+    if (optind+2 != argc) {                                                                         //on verifie que la commande a le bon nombre d'arguments
         fprintf(stderr, "Nombre d'arguments incorrect\n");
         exit(EXIT_FAILURE);
     }
-    hostname = argv[optind];
+    hostname = argv[optind];                                                                        //on recupere les arguments
     port = atoi(argv[optind+1]);
-    receiver(hostname, port, N, format);
+    receiver(hostname, port, N, format);                                                            //on lance la fonction receiver qui consiste à la suite du programme
     exit(EXIT_SUCCESS);
 }
+
+
+
+
 
 
 
